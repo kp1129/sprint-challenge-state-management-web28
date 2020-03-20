4f9ba625bf9afe678ed14f8028944802b69029f8
kp1129 <katya.p.1129@gmail.com>
2020-03-20 09:02:29 -0700

    set up app, redux store

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import {smurfsReducer, initialState} from './reducers/smurfsReducer';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+
+const store = createStore(smurfsReducer, initialState);
+
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>
+
+, document.getElementById("root"));
---
+++ smurfs/src/reducers/smurfsReducer.js
@@ -0,0 +1,13 @@
+// initial state
+export const initialState = {
+    message: "Add more Smurfs using the form below!",
+    smurfs: []
+}
+
+// reducer function
+export const smurfsReducer = (state, action) => {
+    switch(action.type){
+        default:
+            return state;
+    }
+}
